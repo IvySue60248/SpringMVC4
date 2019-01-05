b8a60e24554f4779d6afbc511ca6cc27158c1d88
i505137 <i.xue@sap.com>
2019-01-05 21:03:33 +0800

    增加静态资源映射配置

--- src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
+++ src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -3,13 +3,19 @@ package com.wisely.highlight_springmvc4;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+/**
+ * Spring MVC的定制配置需要我们的配置类继承一个WebMvcConfigurerAdaptor类，并在配置类上使用@EnableWebMvc注解，
+ * 来开启对Spring MVC的配置支持，这样就可以重写这个类的方法，完成常用配置。
+ */
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.wisely.highlight_springmvc4")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -17,5 +23,15 @@ public class MyMvcConfig {
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    /**
+     * 增加静态资源映射
+     * assResourceLocations指的是文件放置的目录，addResourceHnadler指的是对外暴露的访问路径
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
     }
 }
