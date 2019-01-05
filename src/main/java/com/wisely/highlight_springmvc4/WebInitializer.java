1c5321ae3cb4d55deeb2dd3605ef0a1c2913768c
i505137 <i.xue@sap.com>
2019-01-05 20:28:26 +0800

    add spring mvc config

---
+++ src/main/java/com/wisely/highlight_springmvc4/WebInitializer.java
@@ -0,0 +1,26 @@
+package com.wisely.highlight_springmvc4;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * WebApplicationInitializer是Spring提供用来配置Servlet3.0+配置的接口，从而实现了替代web.xml的位置。
+ * 实现此接口将会自动被SpringServletContainerInitializer（用来启动Servlet的容器）获取到
+ */
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyMvcConfig.class);
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
